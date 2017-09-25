235e53c5a734d6d38c26c20e346dadfac2af2f2a
scollins33 <sean_collins@gmx.com>
2017-09-25 19:56:56 -0400

    functionality completed, input validation completed

--- assets/site.js
+++ assets/site.js
@@ -10,6 +10,7 @@ let config = {
 };
 firebase.initializeApp(config);
 
+// create Firebase DB var
 let database = firebase.database();
 
 // listen for submit click
@@ -27,8 +28,21 @@ $('#submitTrain').on('click', function (event) {
         timestamp: firebase.database.ServerValue.TIMESTAMP
     };
 
-    // push object to DB
-    database.ref().push(newTrain);
+    // test inputs for validity
+    let testValid = checkSubmit(newTrain);
+    if (testValid) {
+        // push object to DB
+        database.ref().push(newTrain);
+    }
+    else {
+        console.log('Submission not valid');
+    }
+
+    // clear form data
+    $('#inputTrainName').val('');
+    $('#inputDestination').val('');
+    $('#inputFirstTime').val('');
+    $('#inputFrequency').val('');
 });
 
 // listen for children being added to DB
@@ -36,32 +50,29 @@ $('#submitTrain').on('click', function (event) {
 // automatically generates children when page loaded
 database.ref().on('child_added',
     function (snapshot) {
-        // create jquery tr for new child
-        let trainRow = $('<tr>');
 
-        // while arrive unix less than current unix, add freq
-        // once arrive unix greater than current unix you have next time
-        // subtract now from arrive to get minUntil
         let rightNow = moment();
-        let startTime = moment(snapshot.val().start, 'HH:mm');
+        let trackerTime = moment(snapshot.val().start, 'HH:mm');
         let freqMin = snapshot.val().freq;
 
-        let timeDiff = rightNow.diff(startTime, 'minutes');
-        let remainderMin = timeDiff % freqMin;
-        let minUntil = freqMin - remainderMin;
-        let nextArrive = rightNow.add(remainderMin, 'minutes').format('HH:mm');
+        // add freq time until tracker is later
+        // this handles next train and trains that haven't started
+        // admitted;y more computationally intensive though...
+        while (rightNow.diff(trackerTime, 'minutes') > 0) {
+            trackerTime.add(freqMin, 'minutes');
+        }
 
-        console.log(timeDiff);
-        console.log(freqMin);
-        console.log(minUntil);
+        // calc difference for remaining time
+        let minUntil = trackerTime.diff(rightNow, 'minutes');
 
+        // create jquery tr for new child
+        let trainRow = $('<tr>');
         // add row data
         $(trainRow).append(`<td>${snapshot.val().name}</td>`);
         $(trainRow).append(`<td>${snapshot.val().dest}</td>`);
         $(trainRow).append(`<td>${freqMin}</td>`);
-        $(trainRow).append(`<td>${nextArrive}</td>`);
+        $(trainRow).append(`<td>${trackerTime.format('HH:mm')}</td>`);
         $(trainRow).append(`<td>${minUntil}</td>`);
-
         // add row to DOM
         $('#trainTable').append(trainRow);
     },
@@ -69,3 +80,19 @@ database.ref().on('child_added',
     function (error) {
         console.log(error);
 });
+
+// data validation function
+function checkSubmit (submitted) {
+    // character length check
+    if (submitted.name.length < 1 || submitted.dest.length < 1 ||
+        submitted.start.length < 4 || submitted.name.length < 1) {
+        return false;
+    }
+
+    // freq can't be 0
+    if (submitted.freq == 0) {
+        return false;
+    }
+
+    return true;
+}
